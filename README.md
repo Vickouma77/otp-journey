6b6e7d085ebf3cbf81c4220a1e9c3b6782d76387
Vickouma77 <Vickprestone20@gmail.com>
2026-05-04 14:37:53 +0300

    Getting started with erlang/otp

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # otp-journey
+This is an erlang/OTP project to learn OTP and erlang. It is a simple project that implements a basic OTP applications.
--- ping.erl
+++ ping.erl
@@ -1,1 +1,16 @@
 -module(ping).
+
+-export([run/0, ping/0]).
+
+run() ->
+  Pid = spawn(fun ping/0),
+  Pid ! self(),
+  receive
+    pong -> ok
+  end.
+
+ping() ->
+  receive
+    From -> From ! pong
+  end.
+
