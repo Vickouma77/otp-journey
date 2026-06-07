a2fa8c56a53bf82c8aa62a0b7ab13f7bf1ac6195
Vickouma77 <Vickprestone20@gmail.com>
2026-06-07 21:58:52 +0300

    Add stringme module with vowel counting functionality

---
+++ essentials/recursions/stringme.erl
@@ -0,0 +1,15 @@
+-module(stringme).
+-export([start/0]).
+
+%% Count vowels
+count_vowels(Str) ->
+    count_vowels(string:to_lower(Str), 0).
+
+count_vowels([], Acc) -> Acc;
+count_vowels([H|T], Acc) when H =:= $a; H =:= $e; H =:= $i; H =:= $o; H =:= $u ->
+    count_vowels(T, Acc+1);
+count_vowels([_ | T], Acc) ->
+    count_vowels(T, Acc).
+
+start() ->
+    io:fmt("Vowels in Erlang is great ~p~n", [count_vowels("Erlang is great")]).
